29643117b40fd17025f05b2f1e36e5e3add27208
rooshan1234 <itsrooshan@gmail.com>
2016-01-31 17:58:52 -0600

    Updated onMessageReceived to include a send notification to the device

--- PlatformTask(GCM)/app/src/main/java/com/example/rooshan/gcmimplementation/MyGcmListenerService.java
+++ PlatformTask(GCM)/app/src/main/java/com/example/rooshan/gcmimplementation/MyGcmListenerService.java
@@ -1,7 +1,15 @@
 package com.example.rooshan.gcmimplementation;
 
+import android.app.Notification;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.media.RingtoneManager;
+import android.support.v4.app.NotificationCompat;
 
 import com.google.android.gms.gcm.GcmListenerService;
 
@@ -23,10 +31,36 @@ public class MyGcmListenerService extends GcmListenerService {
         Log.d(CLASSTAG,"Message: " + messageFromGCMSever);
 
         //send a proper notification to device
-        sendNoftifcation();
+        sendNoftifcation(messageFromGCMSever);
     }
 
-    void sendNoftifcation (){
+    private void sendNoftifcation (String message){
+
+        Log.d(CLASSTAG, "Show Notification!");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("GCM Message")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setCategory(Notification.CATEGORY_MESSAGE)
+                .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000})
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(1, notificationBuilder.build());
 
     }
 }
